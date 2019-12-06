5ffde3aa707c9a40de8a1a93b280ee3d5ea231ee
fitkovich1 <fitkovich1@gmai.com>
2019-12-06 17:30:29 +0300

    Updates

---
+++ precache-manifest.8a468ee8bee8a49bdc5f9c7bdc9f820c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5968f08ffa0cbc3ab3f10eebffe7c3ab",
+    "url": "/socialNetwork/index.html"
+  },
+  {
+    "revision": "cf1be6f02177e25ff6af",
+    "url": "/socialNetwork/static/css/main.6df93628.chunk.css"
+  },
+  {
+    "revision": "ce99c96907b3f1232623",
+    "url": "/socialNetwork/static/js/2.f959b085.chunk.js"
+  },
+  {
+    "revision": "cf1be6f02177e25ff6af",
+    "url": "/socialNetwork/static/js/main.5e048ac4.chunk.js"
+  },
+  {
+    "revision": "fb4af6fc242fa7069a81",
+    "url": "/socialNetwork/static/js/runtime-main.f8d3f3b2.js"
+  },
+  {
+    "revision": "ea7658a6e0e720c8fa14f4cbf986e6b7",
+    "url": "/socialNetwork/static/media/user.ea7658a6.jpg"
+  }
+]);
